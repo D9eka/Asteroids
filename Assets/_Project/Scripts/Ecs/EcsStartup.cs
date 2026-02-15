01f487dcd1b55ce720b5a7c17787624f75f638bc
D9eka <120364200+D9eka@users.noreply.github.com>
2026-02-15 11:49:48 +0500

    Add ECS pause support via ITickableSystem
    - Split ECS systems into core (ConfigSync, Destroy) and gameplay groups. Gameplay systems pause on player death through existing PauseSystem.

--- Assets/_Project/Scripts/Ecs/EcsStartup.cs
+++ Assets/_Project/Scripts/Ecs/EcsStartup.cs
@@ -4,6 +4,7 @@ using Asteroids.Scripts.Ecs.Colliders.Services;
 using Asteroids.Scripts.Ecs.Collision.Systems;
 using Asteroids.Scripts.Ecs.Systems;
 using Asteroids.Scripts.Input;
+using Asteroids.Scripts.Pause;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
 using UnityEngine;
@@ -11,12 +12,14 @@ using Zenject;
 
 namespace Asteroids.Scripts.Ecs
 {
-    public class EcsStartup : IInitializable, IFixedTickable, IDisposable
+    public class EcsStartup : IInitializable, IFixedTickable, IDisposable, ITickableSystem
     {
         private readonly DiContainer _container;
         private readonly EcsWorld _world;
         
-        IEcsSystems _systems;
+        IEcsSystems _coreSystems;
+        IEcsSystems _gameplaySystems;
+        private bool _isEnabled;
 
         public EcsStartup(DiContainer container, EcsWorld world)
         {
@@ -26,13 +29,19 @@ namespace Asteroids.Scripts.Ecs
         
         public void Initialize()
         {
-            _systems = new EcsSystems(_world);
+            _coreSystems = new EcsSystems(_world);
+            _gameplaySystems = new EcsSystems(_world);
 #if UNITY_EDITOR
-            _systems.Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem());
+            _coreSystems.Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem());
+            _gameplaySystems.Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem());
 #endif
-            _systems
-                .Add(new InputWriteSystem(_container.Resolve<IPlayerInput>()))
+            _coreSystems
                 .Add(new ConfigSyncSystem(_container.Resolve<IPlayerConfigProvider>()))
+                .Add(new DestroySystem(_container.Resolve<EntityViewRegistry>()))
+                .Init();
+
+            _gameplaySystems
+                .Add(new InputWriteSystem(_container.Resolve<IPlayerInput>()))
                 .Add(new PlayerMovementSystem())
                 .Add(new TargetDirectionSystem())
                 .Add(new LinearMovementSystem())
@@ -40,20 +49,24 @@ namespace Asteroids.Scripts.Ecs
                 .Add(new TargetRotationSystem())
                 .Add(new DamageSystem())
                 .Add(new LifeTimeSystem())
-                .Add(new DestroySystem(_container.Resolve<EntityViewRegistry>()))
                 .Init();
         }
         
         public void FixedTick()
         {
-            _systems?.Run();
+            _coreSystems?.Run();
+            if (_isEnabled) _gameplaySystems?.Run();
         }
-
-
+        
         public void Dispose()
         {
-            _systems.Destroy();
+            _coreSystems.Destroy();
+            _gameplaySystems.Destroy();
             _world.Destroy();
         }
+        
+        public void Enable() => _isEnabled = true;
+        
+        public void Disable() => _isEnabled = false;
     }
 }
